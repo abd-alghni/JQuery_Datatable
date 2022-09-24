ad938abf0e48230fab60bf50c9f56b41e5fcaf63
abd-alghni <aboodowda1@hotmail.com>
2022-09-24 10:27:21 +0300

    Add project files.

---
+++ JQuery_Datatable/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JQuery_Datatable.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JQuery_Datatable.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
---
+++ JQuery_Datatable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JQuery_Datatable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
